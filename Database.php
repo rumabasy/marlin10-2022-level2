f64a68b111141a93f2d38ac7a95a727b1facb76a
Serg <rumabasy@gmail.com>
2022-10-08 01:28:57 +0300

    made func query for some inputs

--- Database.php
+++ Database.php
@@ -2,7 +2,7 @@
 
 class Database {
     private static $instance=null;//подключение, показывает запускался ли конструктор на подключение к базе
-    private $pdo;
+    private $pdo, $query, $error=false, $results, $count;
 
 
     private function __construct() {
@@ -13,21 +13,46 @@ class Database {
             die($exception->getMessage());
         }    
     }
+    
+    public function count(){
+        return $this->count;
+    }
+    
+    public function error(){
+        return $this->error;
+    }
 
+    public function query($sql, $params=[]) {
+        // var_dump($params); die;
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+        if(count($params)){
+            $i=1;
+            foreach ($params as $par) {
+                $this->query->bindValue($i, $par);//встроенная функция 1значит что $params[0]подставляется вместо первого вопроса в запросе
+                $i++;
+            }
+        }
+        if(!$this->query->execute()){
+            $this->error = true;
+        } else {
+            $this->results=$this->query->fetchAll(PDO::FETCH_OBJ);//FETCH_OBJ потомучто мы используем ООП и мы работаем с объектами класса
+            $this->count = $this->query->rowCount();//встроенная функция считает число строк в массиве
+        }
+        return $this ; //так возвращается весь текущий экземпляр объекта, все его параметры
+        // $this->query->execute();
+        // возвращаем массив из объектов
+    }
+    
+    public function results(){
+        return $this->results;
+    }
+    
     public static function getInstance(){
         if(!isset(self::$instance)){    //если не было подключения то запускает конструктор этого класса и делает его        
             self::$instance = new Database();
         }
         return self::$instance;
     }
-    
-    public function query($sql) {
-        $query = $this->pdo->prepare($sql);
-        $query->execute();
-        $result=$query->fetchAll(PDO::FETCH_OBJ);//FETCH_OBJ потомучто мы используем ООП и мы работаем с объектами класса
-        // возвращаем массив из объектов
-        return $result ;
-    }
-    
     
 }
--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <?php
 require_once 'Database.php';
 
-$userz=Database::getInstance()->query('SELECT * FROM userz');
+// $userz=Database::getInstance()->query('SELECT * FROM userz WHERE username="first user"');//first user
+// $userz=Database::getInstance()->query('SELECT * FROM userz WHERE username=?', ['first user']);//first user
+$userz=Database::getInstance()->query('SELECT * FROM userz WHERE username IN (?,?)', ['first user','third user']);//first user third user
 
-var_dump($userz);
-echo '<br>';
+// Database::getInstance()=>get('userz', ['username','=','first user']);
+// Database::getInstance()=>delete('userz', ['username','=','first user']);
 
-foreach ($userz as $user) {
-    echo '<br>'. $user->username;
+
+var_dump($userz->count());
+
+if($userz->error()){
+    echo 'we have an error<br>';
+} else {
+    // echo 'we have no error<br>'; //die;
+    foreach ($userz->results() as $user) {
+        echo '<br>'. $user->username;
+    }
+
 }
+
+echo '<br>';
+echo '<br>';
+var_dump($userz);
+
 //Database::getInstance()->query("SELECT * FROM users WHERE username=?", ['Sergii']);
 //$users=Database::getInstance()->get('users', ['name', '=', 'marlin']);
 //
