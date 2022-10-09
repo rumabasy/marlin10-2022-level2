ca52ee3156239d94cee08fdc14dadef0093823f1
Serg <rumabasy@gmail.com>
2022-10-09 14:49:33 +0300

    made methods: get delete action. and all work)

--- index.php
+++ index.php
@@ -3,18 +3,20 @@ require_once 'Database.php';
 
 // $userz=Database::getInstance()->query('SELECT * FROM userz WHERE username="first user"');//first user
 // $userz=Database::getInstance()->query('SELECT * FROM userz WHERE username=?', ['first user']);//first user
-$userz=Database::getInstance()->query('SELECT * FROM userz WHERE username IN (?,?)', ['first user','third user']);//first user third user
+// $userz=Database::getInstance()->query('SELECT * FROM userz WHERE username IN (?,?)', ['first user','third user']);//first user third user
 
-Database::getInstance()->get('userz', ['username','=','first user']);
-// Database::getInstance()=>delete('userz', ['username','=','first user']);
+// $userz=Database::getInstance()->get0('userz', ['password','=','pass']);
+// $userz=Database::getInstance()->delete0('userz', ['password','=','pass4']);
+// $userz=Database::getInstance()->delete('userz', ['id','=','3']);
+$userz=Database::getInstance()->get('userz', ['password','=','pass9']);
+// Database::getInstance()->delete('userz', ['username','=','fiveth user']);
 
 
-dump($userz->count(),9);
+// dump($userz,9);
 
 if($userz->error()){
     echo 'we have an error<br>';
 } else {
-    // echo 'we have no error<br>'; //die;
     foreach ($userz->results() as $user) {
         echo '<br>'. $user->username;
     }
@@ -23,14 +25,16 @@ if($userz->error()){
 
 echo '<br>';
 echo '<br>';
+
+if(Database::getInstance()->get0('userz', ['username','=','first user'])->count()<5){
+    foreach ($userz->results() as $user) {
+                echo '<br>'. $user->username;
+            }
+}
+
+echo '<br>';
 // dump($userz);
 
-//Database::getInstance()->query("SELECT * FROM users WHERE username=?", ['Sergii']);
-//$users=Database::getInstance()->get('users', ['name', '=', 'marlin']);
-//
-//
-//foreach ($users as $user) {
-//    echo $user->name;
-//}
-//
+
+
 
