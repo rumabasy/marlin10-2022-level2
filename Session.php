b2cbb61b7d3b81f3f3a834d07f525a3ba93af620
Serg <rumabasy@gmail.com>
2022-10-13 12:04:33 +0300

    made class Session lesson12

--- Session.php
+++ Session.php
@@ -15,6 +15,15 @@ class Session {
             unset($_SESSION[$name]);
         }
     }
+    public static function flash($alert, $message=''){
+        if(self::exists($alert) && self::get($alert) !== ''){//если алерт уже существует и мессаж в нем не пустой, то алерт выводится и удаляется
+            $session = self::get($alert);
+            self::delete($alert);
+            return $session;
+        } else {
+            self::put($alert, $message);//если алерт не существует, то он создается с мэссажем
+        }
+    }
 
     public static function get($name){
         return $_SESSION[$name];
